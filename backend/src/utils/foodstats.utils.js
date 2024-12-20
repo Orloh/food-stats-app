6901aadc0292bc2db40a23754b91f7fd2403ce30
Orloh <ohercelli@protonmail.com>
2024-12-20 14:01:19 +0100

    chore: add comments to improve code clarity

--- backend/src/utils/foodstats.utils.js
+++ backend/src/utils/foodstats.utils.js
@@ -7,18 +7,19 @@ export function leastCaloricFoods(numOfFoods,foodList) {
     // iterate through the rest of the foodList
     for (const food of foodList){
         if(sortedFoods.length < numOfFoods) {
+            // insert food into sortedFoods list
             sortedFoods.push(food)
+            // sort sortedFood list in asccending order
+            sortedFoods.sort((a, b) => a.food.nutrients.ENERC_KCAL - b.food.nutrients.ENERC_KCAL)
         }
         // check if the current food has fewer calories than the most caloric food in sortedFoods
         else if(food.food.nutrients.ENERC_KCAL < sortedFoods.at(-1).food.nutrients.ENERC_KCAL){
-            // sort sortedFood list in asccending order
-            sortedFoods.sort((a, b) => a.food.nutrients.ENERC_KCAL - b.food.nutrients.ENERC_KCAL)
             // replace the last element of the sortedFoods list with the current food
             sortedFoods[sortedFoods.length - 1] = food
+            // sort sortedFood list in asccending order
+            sortedFoods.sort((a, b) => a.food.nutrients.ENERC_KCAL - b.food.nutrients.ENERC_KCAL)
         }
     }
-    // sort sortedFood list in asccending order
-    sortedFoods.sort((a, b) => a.food.nutrients.ENERC_KCAL - b.food.nutrients.ENERC_KCAL)
     
     return sortedFoods
 } 
