bcfc822d5a3ff952f2dae1271fe3322b9e571459
Orloh <ohercelli@protonmail.com>
2024-12-20 13:38:40 +0100

    fix: handle when foodList length is less than the numOfFoods

--- backend/src/utils/foodstats.utils.js
+++ backend/src/utils/foodstats.utils.js
@@ -1,27 +1,29 @@
 // find the numOfFoods items with the lowest calorie content from a foodList
 export function leastCaloricFoods(numOfFoods,foodList) {
     
-    // initialize sortedFoods with the first numOfFoods items
-    // sort by their calorie content (ascending) 
-    let sortedFoods = foodList
-        .slice(0,numOfFoods) //take first numOfFoods elements
-        .sort((a, b) => a.food.nutrients.ENERC_KCAL - b.food.nutrients.ENERC_KCAL) // sort by calories (ascending)
+    // initialize and empty sortedFoods list
+    let sortedFoods = []
     
     // iterate through the rest of the foodList
-    for (const food of foodList.slice(numOfFoods)){
-        
+    for (const food of foodList){
+        if(sortedFoods.length < numOfFoods) {
+            sortedFoods.push(food)
+        }
         // check if the current food has fewer calories than the most caloric food in sortedFoods
-        if (food.food.nutrients.ENERC_KCAL < sortedFoods.at(-1).food.nutrients.ENERC_KCAL){
-            // replace the last element of the sortedFoods list with the current food
-            sortedFoods[sortedFoods.length - 1] = food
+        else if(food.food.nutrients.ENERC_KCAL < sortedFoods.at(-1).food.nutrients.ENERC_KCAL){
             // sort sortedFood list in asccending order
             sortedFoods.sort((a, b) => a.food.nutrients.ENERC_KCAL - b.food.nutrients.ENERC_KCAL)
+            // replace the last element of the sortedFoods list with the current food
+            sortedFoods[sortedFoods.length - 1] = food
         }
     }
+    // sort sortedFood list in asccending order
+    sortedFoods.sort((a, b) => a.food.nutrients.ENERC_KCAL - b.food.nutrients.ENERC_KCAL)
     
     return sortedFoods
 } 
 
+// format a list of food objects by extracting their food property.
 export function formatFoodList(foodList) {
     const formattedFoodList = foodList.map((food) => food.food)
     return formattedFoodList
