f50eda50ce7b797b745d3cda88cee08e96ccc693
Orloh <ohercelli@protonmail.com>
2024-12-22 01:29:35 +0100

    feat(search): implemented search functionality on homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,25 @@
 import { useState } from "react";
+import SearchForm from "../components/SearchForm";
+import { getFoodData } from "../services/foodService";
 
 const HomePage = () => {
+    const [foodsData, setFoodsData] = useState([]);
+    const [error, setError] = useState(null);
+
+    const handleSearch = async (food) => { 
+        try {
+            const data = await getFoodData(food);
+            setFoodsData(data);
+            setError(null);
+        } catch (error) {
+            setError(error.message);
+        }
+    };
+
     return (
         <div>
             <h1>Food Calorie App Header</h1>
-            <h3>Search Form</h3>
+            <SearchForm onSearch={handleSearch} />
             <h4>Food Cards</h4>
         </div>
     )
